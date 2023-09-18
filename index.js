efebc04f2e77b8b51ad282423038f6d86f63cc0b
EzatRashid <ezat.b.rashid@gmail.com>
2023-09-18 11:39:24 -0600

    s

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const cards = document.querySelectorAll('.card')
 
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle('show', entry.isIntersecting);
 
-document.addEventListener('DOMContentLoaded',() =>{
-    const observer = new IntersectionObserver(entries =>{
-        entries.forEach(entry =>{
-            entry.target.classList.toggle('show',entry.isIntersecting)
-        })
-        if(entry.isIntersecting){
-            observer.unobserve(entries.target)
-        }
-    })
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target);
+            }
+        });
+    });
 
-    cards.forEach(card =>{
-        observer.observe(card)
-    })
-
-})
-
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
